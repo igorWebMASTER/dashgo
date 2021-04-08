2db75d9c42f0059f0dd66e49427543d94cbd8559
igorWebMASTER <igor_1917@hotmail.com>
2021-04-08 01:17:27 -0300

    feat: finishing the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon} from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Flex, Text, Input, Icon, HStack, Box, Avatar} from '@chakra-ui/react';
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri';
 
 export function Header(){
   return(
@@ -38,13 +38,46 @@ export function Header(){
           <Input 
             color="gray.50"
             variant="unstyled"
-            placeholder="Buscar na plataforma"
             px="4"
             mr="4"
+            placeholder="Buscar na plataforma"
             _placeholder={{ color : 'gray.400' }}
           />
+
           <Icon as={RiSearchLine} />
       </Flex>
+
+      <Flex 
+        align="center"
+        ml="auto"
+      >
+        <HStack 
+          spacing="8"  
+          mx="8" 
+          pr="8" 
+          py="1" 
+          color="gray.300" 
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+
+      <Flex align="center">
+        <Box mr="4" textAlign="right">
+          <Text>Diego Fernandes</Text>
+          <Text color="gray.300" fontSize="small">
+            igor_1917@hotmail.com
+          </Text>
+        </Box>
+
+        <Avatar size="md" name="Diego Fernandes" />
+      </Flex>
+      </Flex>
+
+
     </Flex>
   )
 }
