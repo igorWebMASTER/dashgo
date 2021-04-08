35d23df44ab07efb07563adb5d4bc12fec3d0148
igorWebMASTER <igor_1917@hotmail.com>
2021-04-08 01:30:31 -0300

    feat: added sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,20 @@
 import { Header } from "../components/Header";
+import { Flex } from "@chakra-ui/react";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard(){
   return (
-    <Header />
+    <Flex direction="column" h="100vh" align="center">
+      <Header />
+      <Flex 
+        w="100%" 
+        my="6" 
+        maxWidth={1480} 
+        mx="auto" 
+        px="6"
+      >
+          <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
