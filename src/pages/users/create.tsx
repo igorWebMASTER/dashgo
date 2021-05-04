cd163dd3edc8079f45d7dfe7d60747f52a177099
igorWebMASTER <igor_1917@hotmail.com>
2021-05-03 22:04:00 -0300

    chore: change mutations

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,6 +9,9 @@ import { useMutation  } from 'react-query'
 
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/dist/client/router";
 
 
 type CreateUserFormData = {
@@ -28,19 +31,31 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser(){
-  const createUser = useMutation('createUser')
+  const router = useRouter()
 
-  const { register, handleSubmit, formState} = useForm({
+  const createUser = useMutation(async (user: CreateUserFormData)=> {
+    const response = await  api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    })
+
+    return response.data.user;
+  }, {
+    onSuccess : () => {
+      queryClient.invalidateQueries('users')
+    }
+  })
+
+  const { register, handleSubmit, formState : { errors, isSubmitting }} = useForm({
     resolver: yupResolver(createUserFormSchema)
   })  
 
-  const { errors } = formState;
-
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-      
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    await createUser.mutateAsync(values)
 
-    console.log(values)
+    router.push('/users')
   }
 
   return (
@@ -105,7 +120,7 @@ export default function CreateUser(){
                   <Button  
                       type="submit"
                       colorScheme="pink"
-                      isLoading={formState.isSubmitting}
+                      isLoading={isSubmitting}
                   >
                     Salvar
                   </Button>
